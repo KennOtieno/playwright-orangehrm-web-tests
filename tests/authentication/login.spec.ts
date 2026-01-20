59de58a7496978d3dc3b58f788a2c9e528002701
KennOtieno <otiken001@gmail.com>
2026-01-20 18:05:41 +0300

    Auth

--- tests/authentication/login.spec.ts
+++ tests/authentication/login.spec.ts
@@ -6,13 +6,13 @@ test('authentication is succesful and dashboatd is displayed', async ({ page }) 
   // Input valid username and password then press login buttp
   const username = await page.getByPlaceholder('Username');
   const password = await page.getByPlaceholder('Password');
-  const loginButton = await page.getByRole('button', {name: Login});
+  const loginButton = await page.getByRole('button', {name: 'Login'});
 
   // Next step is to fill them up
   await username.fill('Admin');
   await password.fill('admin123');
   await loginButton.click();
-  
+
 // If it has this, it's a pass
   await expect(page).toHaveURL(/dashboard/);
 
