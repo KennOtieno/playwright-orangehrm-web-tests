40e4f6cbb41b8dc3af42a3407228a84f6992c086
KennOtieno <otiken001@gmail.com>
2026-01-20 18:02:18 +0300

    Authentication

--- tests/authentication/login.spec.ts
+++ tests/authentication/login.spec.ts
@@ -1,19 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test('authentication is succesful and dashboatd is displayed', async ({ page }) => {
-  await page.goto('https://https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
   // Input valid username and password then press login buttp
-  const username = await page.locator('Username');
-  const password = await page.locator('Password');
-  const loginButton = await page.locator('Login');
+  const username = await page.getByPlaceholder('Username');
+  const password = await page.getByPlaceholder('Password');
+  const loginButton = await page.getByRole('button', {name: Login});
 
   // Next step is to fill them up
-  username.fill('Admin');
-  password.fill('admin123');
-  loginButton.click();
+  await username.fill('Admin');
+  await password.fill('admin123');
+  await loginButton.click();
+  
+// If it has this, it's a pass
+  await expect(page).toHaveURL(/dashboard/);
 
-  await expect(page).toHaveURL(/viewSystemUsers/);
+  await expect(page.getByRole('heading', {name: 'Dashboard'})).toBeVisible();
+
+  await expect(page.locator('.oxd-userdropdown-name')).toBeVisible();
 
 
 
