04e83c1f01b032c41418e6ec3c52bbe8168d4a4d
KennOtieno <otiken001@gmail.com>
2026-01-21 15:09:50 +0300

    Authentication

--- tests/authentication/logout.spec.ts
+++ tests/authentication/logout.spec.ts
@@ -1,10 +1,31 @@
 import { test, expect } from '@playwright/test';
 
 test('User is logged out', async ({page}) => {
-    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    const useProfileDropdown = await page.getByRole('list', {name: 'mandapavan vasudev'});
-    const LogoutButton = await page.getByRole('button', {name: 'Logout'});
+    // Log in first
 
-    expect (page).toHaveURL('/login/');
+    const username = await page.getByPlaceholder('Username');
+    const password = await page.getByPlaceholder('Password');
+    const loginButton = await page.getByRole('button', {name: 'Login'});
+
+    await username.fill('Admin');
+    await password.fill('admin123');
+    await loginButton.click();
+
+    await expect(page).toHaveURL(/dashboard/);
+
+    // Click user dropdown and then click the log out button
+    const userDropdown = await page.locator('.oxd-userdropdown-menu');
+    const logoutButton = await page.getByRole('button', {name: 'Logout'});
+
+    await userDropdown.click();
+
+    await expect(page).toHaveURL(/dashboard/);
+
+    // Click the log out button
+    await logoutButton.click();
+
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
 });
