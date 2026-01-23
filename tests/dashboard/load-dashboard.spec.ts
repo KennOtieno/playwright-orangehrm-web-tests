fb6078ef0bb10bcadfa370449024f6d72d57b74d
KennOtieno <otiken001@gmail.com>
2026-01-23 09:17:28 +0300

    Dashboard

--- tests/dashboard/load-dashboard.spec.ts
+++ tests/dashboard/load-dashboard.spec.ts
@@ -3,16 +3,16 @@ import { loginPage} from '../../pages/login.pages';
 
 test('Dashboard loads after login', async ({page}) => {
 
-    const LoginPage = new loginPage(page);
+    const LoginPage = await new loginPage(page);
     await LoginPage.goto();
     await LoginPage.login('Admin', 'admin123');
 
     // Ensure URL has /dashboad
-    await expect(page).toHaveURL(/dashboad/);
+    await expect(page).toHaveURL(/dashboard/);
 
     // To have heading Dashboard
-    await expect(page.getByRole('heading', {name: 'Dashbard'})).toBeVisible();
-    
+    await expect(page.getByRole('heading', {name: 'Dashboard'})).toBeVisible();
+
 
 
 });
