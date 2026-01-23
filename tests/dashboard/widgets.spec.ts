26466ffbbc5b1db4039f3ed944dba44243ccf649
KennOtieno <otiken001@gmail.com>
2026-01-23 14:45:56 +0300

    Blocker - Seeking QA Automation help.

--- tests/dashboard/widgets.spec.ts
+++ tests/dashboard/widgets.spec.ts
@@ -1,28 +1,26 @@
-/*
-import {test, expect} from '@playwright/test';
-import { loginPage } from '../../pages/login.pages';
+// import {test, expect} from '@playwright/test';
+// import { loginPage } from '../../pages/login.pages';
 
-test('All dashboard widgets are present', async ({page})  => {
-    const LoginPage = new loginPage(page);
-    await LoginPage.goto()
-    await LoginPage.login('Admin', 'admin123');
+// test('All dashboard widgets are present', async ({page})  => {
+   // const LoginPage = new loginPage(page);
+    // await LoginPage.goto()
+    // await LoginPage.login('Admin', 'admin123');
 
-    await expect(page).toHaveURL(/dashboard/);
+    // await expect(page).toHaveURL(/dashboard/);
 
     // User has to see the widgets. According to the expected results:
     // All default widgets are displayed
-    const widgets = page.locator('.oxd-dashboard-widget');
+    // const widgets = page.locator('.oxd-dashboard-widget');
 
     // Waiting for at least to pop up as in render
-    widgets.first().waitFor({state: 'visible'});
+    // widgets.first().waitFor({state: 'visible'});
 
 
-    const widgetCount = await widgets.count();
+    // const widgetCount = await widgets.count();
 
-    expect (widgetCount).toBeGreaterThan(0);
+    // expect (widgetCount).toBeGreaterThan(0);
     
-});
-
+//});
 // Expected rsults are: 
 // All default widgets are displayed
 // No broken or empty widgets
